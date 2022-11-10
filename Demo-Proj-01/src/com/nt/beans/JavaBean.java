9f0e908ea2c2471709b87eb65a7976403a2502dc
Dev-01 <TL@gmail.com>
2022-11-10 19:34:05 +0530

    initial modify

---
+++ Demo-Proj-01/src/com/nt/beans/JavaBean.java
@@ -0,0 +1,8 @@
+package com.nt.beans;
+
+public class JavaBean {
+	
+	public String Print() {
+		return "Bean class Print() method..";
+	}
+}
---
+++ Demo-Proj-01/src/com/nt/test/DemoProjTest.java
@@ -0,0 +1,17 @@
+package com.nt.test;
+
+import com.nt.beans.JavaBean;
+
+public class DemoProjTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaBean jb=null;
+		
+		jb=new JavaBean();
+		
+		System.out.println(jb.Print());
+
+	}
+
+}
